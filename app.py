d7839811a6d9a363d11f7e58be0751f50b2be135
quintin_wu@outlook.com <quintin_wu@outlook.com>
2023-02-05 20:27:25 -0500

    created my first Flask app

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'My home page'
 '''
 # Routing for your application.
 # Put your routes below this comment
